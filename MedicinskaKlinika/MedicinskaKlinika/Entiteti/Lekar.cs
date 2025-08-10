243d6501c8e148b49c50003839c0e05bf983a33c
djordje235 <sdjordje235@gmail.com>
2025-08-10 17:54:26 +0200

    poruka

---
+++ MedicinskaKlinika/MedicinskaKlinika/Entiteti/Lekar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicinskaKlinika.Entiteti
+{
+    internal class Lekar : Zaposlen
+    {
+        public virtual int JMBG {  get; set; }
+        public virtual String Specijalizacija { get; set; }
+        public virtual int BrLicence { get; set; }
+    }
+}
---
+++ MedicinskaKlinika/MedicinskaKlinika/Entiteti/PomocniRadnik.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicinskaKlinika.Entiteti
+{
+    internal class PomocniRadnik : Zaposlen
+    {
+        public virtual int JMBG { get; set; }
+        public virtual String OblastRada {  get; set; }
+        public virtual String Sertifikat {  get; set; }
+    }
+}
