a2ee14491ea7a3d5847e27c329f7e37b239d3d79
djordje235 <sdjordje235@gmail.com>
2025-08-10 18:16:55 +0200

    poruka

---
+++ MedicinskaKlinika/MedicinskaKlinika/Entiteti/BrTelefonaPacijenta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicinskaKlinika.Entiteti
+{
+    internal class BrTelefonaPacijenta
+    {
+        public virtual int JmbgPacijenta { get; set; }
+        public virtual int BrojTelefona {  get; set; }
+    }
+}
---
+++ MedicinskaKlinika/MedicinskaKlinika/Entiteti/BrTelefonaZaposlenog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicinskaKlinika.Entiteti
+{
+    internal class BrTelefonaZaposlenog
+    {
+        public virtual int JmbgZaposlenog {  get; set; }
+        public virtual int BrojTelefona { get; set; }
+    }
+}
